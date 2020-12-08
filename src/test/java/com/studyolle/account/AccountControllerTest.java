7204dafeb8c07f0ddf15c0704f4e64895e8b297a
youngsunshin94 <shinysys94@naver.com>
2020-12-08 14:20:24 +0900

    회원 가입 패스워드 인코딩

--- src/test/java/com/studyolle/account/AccountControllerTest.java
+++ src/test/java/com/studyolle/account/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.studyolle.account;
 
+import com.studyolle.domain.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,6 +64,10 @@ class AccountControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/"));
 
+        Account account = accountRepository.findByEmail("keesun@email.com");
+
+        assertNotNull(account);
+        assertNotNull(account.getPassword(),"12345678");
         assertTrue(accountRepository.existsByEmail("keesun@email.com"));
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
 
